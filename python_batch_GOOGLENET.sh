524b75e614795ebf8a5e568c3635b39cb732bc65
Pedro <hs.pedro@hotmail.com>
2016-05-31 15:39:51 +0200

    new model googlenet_resized added

--- python_batch_GOOGLENET.sh
+++ python_batch_GOOGLENET.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=23:00:00
+#SBATCH --time=26:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=caffe_alexnet
-#SBATCH --mem=4000
+#SBATCH --mem=8000
 module add caffe/rc3-foss-2016a-CUDA-7.5.18
-caffe train -solver models/foodCAT_googlenet_food101_500/solver.prototxt -weights models/foodCAT_googlenet_food101_500/snapshots/ss_foodCAT_googlenet_food101_train_iter_490000.caffemodel
+caffe train -solver models/googlenet_resized/solver.prototxt -weights models/googlenet_resized/snapshots/foodRecognition_googlenet_finetunning_v2_1_iter_448000.caffemodel
